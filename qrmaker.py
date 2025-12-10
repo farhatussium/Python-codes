bba456a8b387c4b974d42cb04076f3099f3d2868
Md Farhatus Sium <252-35-001@diu.edu.bd>
2025-12-10 21:03:02 +0600

    Remove deprecated scripts: app.py, qrmaker.py, todo.py, and todo_app2.py

--- qrmaker.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-
-link = input("Enter the link/URL: ")
-
-qr = qrcode.make(link)
-
-qr.save("qrcode.png")
-print("QR Code saved as qrcode.png")
-qr.show()
--- todo.py
+++
@@ -1,9 +0,0 @@
-todo=[]
-while True:
-    action=input("Add/view/quit?").lower()
-    if action=="add":
-        todo.append(input("Enter a task: "))
-    elif action=="view":
-        print("\nTasks:", todo if todo else "No tasks added yet.")
-    elif action=="quit":
-        break
